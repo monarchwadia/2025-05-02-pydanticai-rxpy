8eb465c2f3c18045ce02fdcdfab7350bd0ac5d5e
Monarch Wadia <monarchwadia@gmail.com>
2025-05-05 09:28:57 -0400

    Improvements

--- main.py
+++ main.py
@@ -2,24 +2,34 @@ import reactivex as rx
 from reactivex import operators as ops
 
 def main():
-    source = rx.of("Hello from 2025-05-02-pydanticai-rxpy!")
+    source = rx.of(
+        # don't tell me how to live my life
+        "don't",
+        "tell",
+        "me",
+        "how",
+        "to",
+        "live",
+        "my",
+        "life"
+    )
     composed = source.pipe(
         ops.do_action(
             on_next=lambda x: print(f"Original: {x}"),
             on_error=lambda e: print(f"Error: {e}"),
-            on_completed=lambda: print("Completed!"),
+            on_completed=lambda: print("Completed: #1"),
         ),
         ops.map(lambda x: x.upper()),
         ops.do_action(
             on_next=lambda x: print(f"Uppercase: {x}"),
             on_error=lambda e: print(f"Error: {e}"),
-            on_completed=lambda: print("Completed!"),
+            on_completed=lambda: print("Completed: #2"),
         )
     )
     composed.subscribe(
         on_next=lambda x: print(f"Final Output: {x}"),
         on_error=lambda e: print(f"Error: {e}"),
-        on_completed=lambda: print("Completed!"),
+        on_completed=lambda: print("Completed: #3"),
     )
 
 
