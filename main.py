ba5656d309c4f76b28aca36f33a7c725eb8be037
Monarch Wadia <monarchwadia@gmail.com>
2025-05-05 09:26:01 -0400

    Simple hello world example

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import reactivex as rx
+from reactivex import operators as ops
+
 def main():
-    print("Hello from 2025-05-02-pydanticai-rxpy!")
+    source = rx.of("Hello from 2025-05-02-pydanticai-rxpy!")
+    composed = source.pipe(
+        ops.do_action(
+            on_next=lambda x: print(f"Original: {x}"),
+            on_error=lambda e: print(f"Error: {e}"),
+            on_completed=lambda: print("Completed!"),
+        ),
+        ops.map(lambda x: x.upper()),
+        ops.do_action(
+            on_next=lambda x: print(f"Uppercase: {x}"),
+            on_error=lambda e: print(f"Error: {e}"),
+            on_completed=lambda: print("Completed!"),
+        )
+    )
+    composed.subscribe(
+        on_next=lambda x: print(f"Final Output: {x}"),
+        on_error=lambda e: print(f"Error: {e}"),
+        on_completed=lambda: print("Completed!"),
+    )
 
 
 if __name__ == "__main__":
